971bc2c9d7d4d7ec6166cf0500cba133b26d7a48
brafit <100429071@alumnos.uc3m.es>
2024-01-02 09:35:14 +0100

    Dump Sql

---
+++ dump.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `pokemons` (
+                            `id` int(11) NOT NULL AUTO_INCREMENT,
+                            `name` varchar(79) NOT NULL,
+                            `url` VARCHAR(200) DEFAULT NULL,
+                            PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=724 DEFAULT CHARSET=UTF8;
+
+
+ALTER TABLE pokemons AUTO_INCREMENT = 0;
